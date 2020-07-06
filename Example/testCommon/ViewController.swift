3994f98e9cc5856b4935b857ce8b7e324efc3ae4
yu nie <nieyuchina@gmail.com>
2020-07-06 08:17:24 +0800

    test example

--- Example/testCommon/ViewController.swift
+++ Example/testCommon/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import testCommon
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        printABC.printABC()
     }
 
     override func didReceiveMemoryWarning() {
